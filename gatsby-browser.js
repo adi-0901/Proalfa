03ae1d3d06954c99b7604a2255310273a91a3f85
Kaustubh <kaustubhpb@gmail.com>
2023-06-27 22:57:12 +0530

    fix: on refresh scroll top

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -16,63 +16,78 @@ export const wrapPageElement = ({ element }) => {
   return <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
 }
 
+// export const shouldUpdateScroll = ({
+//   prevRouterProps,
+//   routerProps: { location },
+//   getSavedScrollPosition,
+// }) => {
+//   // transition duration from `layout.js` * 1000 to get time in ms
+
+//   // * 2 for exit + enter animation
+//   const prevPathname =
+//     prevRouterProps !== undefined
+//       ? prevRouterProps.location.pathname
+//       : undefined
+
+//   const TRANSITION_DELAY = 1.2 * 1000 * 2
+
+//   console.log(location)
+//   console.log(prevPathname)
+
+//   // if it's a "normal" route
+
+//   if (location.action === "PUSH") {
+//     window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
+//   }
+
+//   // if we used the browser's forwards or back button
+//   else {
+//     const savedPosition = getSavedScrollPosition(location) || [0, 0]
+
+//     const projectRegex = /\/project\/.*/
+
+//     switch (true) {
+//       case location.pathname === "/":
+//         window.setTimeout(() => window.scrollTo(...savedPosition), 10)
+//         break
+//       case projectRegex.test(location.pathname):
+//         console.log(typeof prevPathname)
+//         console.log(projectRegex)
+//         const prevPageisProjectPage = projectRegex.test(prevPathname)
+//         console.log(prevPathname, prevPageisProjectPage)
+//         if (prevPageisProjectPage) {
+//           console.log("previous page was a project page")
+//           window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+//         } else {
+//           window.setTimeout(
+//             () => window.scrollTo({ top: 0, left: 0 }),
+//             TRANSITION_DELAY / 2
+//           )
+//         }
+
+//         break
+//       default:
+//         window.setTimeout(
+//           () => window.scrollTo(...savedPosition),
+//           TRANSITION_DELAY
+//         )
+//         break
+//     }
+//   }
+
+//   return false
+// }
+
 export const shouldUpdateScroll = ({
-  prevRouterProps,
   routerProps: { location },
   getSavedScrollPosition,
 }) => {
-  // transition duration from `layout.js` * 1000 to get time in ms
-
-  // * 2 for exit + enter animation
-  const prevPathname =
-    prevRouterProps !== undefined
-      ? prevRouterProps.location.pathname
-      : undefined
-
-  const TRANSITION_DELAY = 1.2 * 1000 * 2
-
-  console.log(location)
-  console.log(prevPathname)
-
-  // if it's a "normal" route
-
-  if (location.action === "PUSH") {
-    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
-  }
-
-  // if we used the browser's forwards or back button
-  else {
-    const savedPosition = getSavedScrollPosition(location) || [0, 0]
-
-    const projectRegex = /\/project\/.*/
-
-    switch (true) {
-      case location.pathname === "/":
-        window.setTimeout(() => window.scrollTo(...savedPosition), 10)
-        break
-      case projectRegex.test(location.pathname):
-        console.log(typeof prevPathname)
-        console.log(projectRegex)
-        const prevPageisProjectPage = projectRegex.test(prevPathname)
-        console.log(prevPathname, prevPageisProjectPage)
-        if (prevPageisProjectPage) {
-          console.log("previous page was a project page")
-          window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-        } else {
-          window.setTimeout(
-            () => window.scrollTo({ top: 0, left: 0 }),
-            TRANSITION_DELAY / 2
-          )
-        }
-
-        break
-      default:
-        window.setTimeout(
-          () => window.scrollTo(...savedPosition),
-          TRANSITION_DELAY
-        )
-        break
-    }
+  const { pathname } = location
+  // list of routes for the scroll-to-top-hook
+  const scrollToTopRoutes = [`/privacy-policy`, `/page-2`]
+  // if the new route is part of the list above, scroll to top (0, 0)
+  if (scrollToTopRoutes.indexOf(pathname) !== -1) {
+    window.scrollTo(0, 0)
   }
 
   return false
