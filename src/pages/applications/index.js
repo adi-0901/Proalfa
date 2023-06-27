eee94e305c4a4744b3ce42a9cff9b5aaefe4a1fb
Kaustubh <kaustubhpb@gmail.com>
2023-06-27 23:06:43 +0530

    feat: application - text added

--- src/pages/applications/index.js
+++ src/pages/applications/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import Layout from '../../components/Layout'
-import LottieApplications from '../../assets/lottie/proalfa_3.4.json'
+import LottieApplications from '../../assets/lottie/proalfa_3.5.json'
 import LottieMedia from '../../components/lottie/LottieMedia'
 import { useState } from 'react'
 import CustomInput from '../../components/CustomInput'
