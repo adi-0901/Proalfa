f92a40f76d7cb4aac7d7e6882a35f2d1b3a3bdd9
Kaustubh <kaustubhpb@gmail.com>
2023-09-28 02:16:51 +0530

    commented header animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,9 +47,9 @@ const Header = () => {
 
   return (
     <HeaderNav
-      initial={{ y: -72, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
+      // initial={{ y: -72, opacity: 0 }}
+      // animate={{ y: 0, opacity: 1 }}
+      // transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
     >
 
       <Container>
